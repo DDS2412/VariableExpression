b3652b35c22624c1cca3c67c27354ecef01abbff
DDS2412 <dds2412@mail.ru>
2019-11-19 12:14:56 +0300

    Рефакторинг ввода Json файла

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,11 +3,35 @@ import model.Node;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class Program {
     public static void main(final String[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        Node node = mapper.readValue(new File(args[0]), Node.class);
+        Program program = new Program();
+        File jsonFile = program.getFileFromResources(args[0]);
+        Node node =program.getNodeFromJson(jsonFile);
         System.out.println(node);
+    }
+
+    private Node getNodeFromJson(File file){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(file, Node.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return new Node();
+    }
+
+    private File getFileFromResources(String fileName){
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return new File(resource.getFile());
+        }
     }
 }
--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -2,12 +2,20 @@ package model;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import model.operations.Operation;
 
 @Data
 @Accessors(chain = true)
 public class Node {
     private Node left;
     private Node right;
-    private String operation;
+    private Operation operation;
     private String value;
+
+    public Node() {}
+
+    public Node(Operation operation) {
+        this.operation = operation;
+    }
+
 }
